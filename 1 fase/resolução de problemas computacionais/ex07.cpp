e9943746b9e6878415f1511d93648b1c5f884019
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-19 19:57:15 -0300

    Add comments for weight calculation program

--- 1 fase/resolução de problemas computacionais/ex07.cpp
+++ 1 fase/resolução de problemas computacionais/ex07.cpp
@@ -1,3 +1,6 @@
+//Faça um programa que receba o peso de uma pessoa, calcule e mostre: 1.
+//a. o novo peso, se a pessoa engordar 15% sobre o peso digitado; 2.
+//b. o novo peso, se a pessoa emagrecer 20% sobre o peso digitado;
 #include<stdio.h>
 main()
 {
