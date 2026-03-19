ba855b504563367b8ef8c69ea5ea78af05f2f9ea
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-19 19:39:29 -0300

    Update ex7.cpp

--- 1 fase/resolução de problemas computacionais/ex7.cpp
+++ 1 fase/resolução de problemas computacionais/ex7.cpp
@@ -3,7 +3,7 @@ main()
 {
     float peso, engorda, emagrece;
     printf("digite o seu peso: ");
-    scanf("%f", &peso);
+    scanf("%f", &peso);  //%f pq é float
     emagrece = peso*0.8;
     engorda = peso*1.15;
     printf("se voce engordar 15%% ficara com: %.2f", engorda);
