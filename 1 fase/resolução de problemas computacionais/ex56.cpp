9491445b3dcc5ea2ce0b320ffb6458c38c4d0b45
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-19 21:07:52 -0300

    Create ex56.cpp

---
+++ 1 fase/resolução de problemas computacionais/ex56.cpp
@@ -0,0 +1,18 @@
+/* Pedro comprou um saco de ração com peso em quilos. Pedro possui dois gatos para os quais fornece
+a quantidade de ração em gramas. Elabore um algoritmo que receba o peso do saco de ração e
+quantidade de ração fornecida para cada gato. Calcule e mostre quanto restará de ração no saco após
+cinco dias. */
+
+#include<stdio.h>
+main()
+{
+	float saco, qtd1, qtd2;
+	printf("digite o peso do saco em kg: ");
+	scanf("%f", &saco);
+	printf("digite a qtd de racao pro gato 1: ");
+	scanf("%f", &qtd1);
+	printf("digite a qtd de racao pro gato 2: ");
+	scanf("%f", &qtd2);
+	
+	printf("resto de racao no saco dps de 5 dias: %.2f", (saco - ((qtd1+qtd2)*0.005)));
+}
