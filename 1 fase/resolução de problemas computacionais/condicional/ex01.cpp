f398a071e1e78f750ac6e4c1763ccc696e0259c5
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-26 19:45:19 -0300

    Update ex01.cpp

--- 1 fase/resolução de problemas computacionais/condicional/ex01.cpp
+++ 1 fase/resolução de problemas computacionais/condicional/ex01.cpp
@@ -1,1 +1,13 @@
+//Elabore um algoritmo que leia um número, e se ele for maior do que 20, imprimir a metade desse número.
 
+#include<stdio.h>
+main()
+{
+	float num;
+	printf("digite um numero: ");
+	scanf("%f", &num);
+	
+	if (num > 20){
+		printf("metade dele: %.2f", (num/2));
+	}
+}
