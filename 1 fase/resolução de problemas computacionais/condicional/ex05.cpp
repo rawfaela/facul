7d86bb1914421f94e3e7632a3f07cfbc4cea7a90
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-03-26 20:08:54 -0300

    Create ex05.cpp

---
+++ 1 fase/resolução de problemas computacionais/condicional/ex05.cpp
@@ -0,0 +1,18 @@
+//Elabore um algoritmo que leia dois números e imprima o maior número.
+
+#include<stdio.h>
+main()
+{
+	float num1, num2;
+	printf("digite o primeiro numero: ");
+	scanf("%f", &num1);
+	printf("digite o segundo numero: ");
+	scanf("%f", &num2);
+	
+	if (num1>num2){
+		printf("%.2f", num1);
+	}
+	else{
+		printf("%.2f", num2);
+	}
+}
