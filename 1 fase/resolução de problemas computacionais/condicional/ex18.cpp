103e3f74b439aab9463796aa3875cad64c037ddf
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-04-09 21:22:29 -0300

    Create ex18.cpp

---
+++ 1 fase/resolução de problemas computacionais/condicional/ex18.cpp
@@ -0,0 +1,20 @@
+/* Faça um algoritmo que receba:
+  • O código do produto comprado; e
+  • A quantidade comprada do produto.
+Calcule e mostre:
+  • O preço unitário do produto comprado, seguindo a Tabela I;
+  • O preço total da nota;
+  • O valor do desconto, seguindo a Tabela II e aplicado sobre o preço total da nota; e
+  • O preço final da nota depois do desconto.
+TABELA I
+CÓDIGO       PREÇO
+1 a 10      R$ 10,00
+11 a 20     R$ 15,00
+21 a 30     R$ 20,00
+
+TABELA II
+PREÇO TOTAL DA NOTA                       % DESCONTO
+Até R$ 250,00 (não incluído)                  5%
+Entre R$ 250,00 e R$ 500,00 (inclusive)      10%
+Acima de R$ 500,00                           15%  */
+
