230f23ddd12cf1b79e98d012513beaed8c9f1064
Rafaela Amboni Ghellere <rafaelaaghellere@gmail.com>
2026-05-28 21:26:36 -0300

    Create exx15.cpp

---
+++ 1 fase/resolução de problemas computacionais/repetição/exx15.cpp
@@ -0,0 +1,13 @@
+/*  Faça um programa que receba os dados a seguir de vários produtos: preço unitário, país de origem
+(1 – Estados Unidos; 2 — México; e 3 — outros), meio de transporte (T — terrestre; F — fluvial; e A —
+aéreo), carga perigosa (S — sim; N — não), finalize a entrada de dados com um preço inválido, ou seja,
+menor ou igual a zero. O programa deve calcular e mostrar os itens a seguir.
+• O valor do imposto, usando a tabela a seguir.
+Preço unitário Imposto
+• O valor do transporte usando a tabela a seguir.
+
+• O valor do seguro, usando a regra a seguir.
+Os produtos que vêm do México e os produtos que utilizam transporte aéreo pagam metade do
+valor do seu preço unitário como seguro.
+• O preço final, ou seja, preço unitário mais imposto mais valor do transporte mais valor do seguro.
+• O total dos impostos. */
